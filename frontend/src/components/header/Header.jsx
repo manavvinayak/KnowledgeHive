e1d4810fc1f83bc4c059a480de2c88b4b63ee603
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-05-07 21:12:33 +0530

    Project

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -7,6 +7,7 @@ const Header = ({ isAuth }) => {
   return (
     <header>
       <div className="logo">LearnInst</div>
+      <div className="logo-img"> <Link to={"/"}> <img src="https://img.freepik.com/premium-vector/online-education-logo-designs-concept-computer-book-logo-designs-template_567288-137.jpg?w=2000" alt="image" /> </Link>  </div>
 
       <div className="link">
         <Link to={"/"}>Home</Link>
